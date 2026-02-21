e30b3c6d21ba3d2b8371550347fc683fb22279d5
Coburn <rcw@peak.org>
2026-02-20 18:55:47 -0800

    add license headers

--- src/sensor_silo/polynomial.py
+++ src/sensor_silo/polynomial.py
@@ -1,3 +1,20 @@
+#
+# polynomial.py - a specialized equation to scale a sensors raw value.
+#                 part of the python sensor silo project.
+#
+# Copyright (c) 2026 Coburn Wightman
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+
 import datetime
 
 from . import procedure
--- src/sensor_silo/quantity.py
+++ src/sensor_silo/quantity.py
@@ -1,3 +1,20 @@
+#
+# quantity.py - a naive container for a value with its name and units.
+#               part of the python sensor silo project.
+#
+# Copyright (c) 2026 Coburn Wightman
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+
 from . import shell
 
 class Quantity(): # Parameter?
